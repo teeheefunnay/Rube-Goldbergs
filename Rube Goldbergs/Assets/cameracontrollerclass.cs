427e9a9c36ba649e897ac6680928d47c940559a6
Tiffany Shu <tzs207@nyu.edu>
2016-11-15 00:54:21 -0500

    Update

---
+++ Rube Goldbergs/Assets/cameracontrollerclass.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameracontrollerclass : MonoBehaviour {
+
+	public GameObject[] Cameras;
+	public AudioSource sfxSound;
+
+	private int currentCam;
+
+	void Start () {
+
+		DeactivateCameras();
+		Cameras[0].SetActive(true);
+		currentCam = 0;
+
+	}
+
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+
+			DeactivateCameras();
+
+			if (currentCam < Cameras.Length - 1) {
+				currentCam +=1;
+			} else {
+				currentCam = 0;
+			}
+
+
+			Cameras[currentCam].SetActive(true);
+
+		}
+
+	}
+
+	public void activateCamera(int _camNumber){
+
+		Cameras[_camNumber - 1].SetActive(true);
+		currentCam = _camNumber - 1;
+	}
+
+	public void DeactivateCameras()
+	{
+		for (int i = 0; i < Cameras.Length; i++)
+		{
+			Cameras[i].SetActive(false);
+		}
+
+	}
+}
---
+++ Rube Goldbergs/Assets/cameratrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTriggerClass : MonoBehaviour {
+
+	public int camNumber;
+
+	public cameracontrollerclass controller;
+
+	void OnTriggerEnter(Collider other){
+
+		controller.DeactivateCameras();
+		controller.activateCamera(camNumber);
+
+	}
+}
+	
